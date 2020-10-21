b6e0d98a88bf954fbf168059426284b791eb1688
curtmanning <curtmanning@ltgc.com>
2020-10-21 10:58:20 -0600

    updated python

--- testcase.sh
+++ testcase.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 echo $1 $2
 echo "Test GATest 1.16 "
-var=$(which python)
+var=$(which python3)
 which python
 pwd
 echo "This is the pwd $(pwd)"
 echo "This is the ls in /__t/Python/3.7.9/x64   $(ls -l /__t/Python/3.7.9/x64)"
-/__t/Python/3.7.9/x64/python -m pip install --upgrade pip
+/__t/Python/3.7.9/x64/python3 -m pip install --upgrade pip
 pip install pytest
 pip install selenium
 pip install allure-pytest
