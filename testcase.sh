31ba5f9c9f70f3a3e8e02cc67ded4c3c417b4e69
curtmanning <curtmanning@ltgc.com>
2020-10-21 16:17:51 -0600

    updated script

--- testcase.sh
+++ testcase.sh
@@ -5,17 +5,19 @@ var=$(which python3)
 which python
 pwd
 echo "This is the pwd $(pwd)"
-echo "This is the ls in /__t/Python/3.7.9/x64   $(ls -l /__t/Python/3.7.9/x64)"
-/__t/Python/3.7.9/x64/python3 -m pip install --upgrade pip
-pip install pytest
-pip install selenium
-pip install allure-pytest
-pip install pytest-ordering
-pip install ddt
-pip install pytest-html
-pip install flask
-pip install requests
+echo "This is the ls in /__t/Python/3.7.9/   $(ls -l /__t/Python/3.7.9/)"
+/__t/Python/3.7.9/x64/python3 -m pip3 install --upgrade pip
+pip3 install pytest
+pip3 install selenium
+pip3 install allure-pytest
+pip3 install pytest-ordering
+pip3 install ddt
+pip3 install pytest-html
+pip3 install flask
+pip3 install requests
 echo "installed pip stuff "
 var1=$(pwd)
 echo "$var1"
+var2=(ls -l)
+echo "$var2"
 echo "finished ls -l ls -l /home/runner/work/"
