79dd6e25c57f3ba57a961608dd1461e4d9862dcc
curtmanning <curtmanning@ltgc.com>
2020-10-21 09:34:32 -0600

    new script

---
+++ testcase.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+echo $1 $2
+echo "Test GATest 1.16 "
+python -m pip install --upgrade pip
+pip install pytest
+pip install selenium
+pip install allure-pytest
+pip install pytest-ordering
+pip install ddt
+pip install pytest-html
+pip install flask
+pip install requests
+ls -l /home/runner/work/
